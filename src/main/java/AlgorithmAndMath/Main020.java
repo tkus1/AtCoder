25e97467668c47a24cc899b294d58941a751c0e1
tkus1 <146398096+tkus1@users.noreply.github.com>
2023-12-11 14:04:13 -0800

    Revise AlgorithmAndMath/020

--- src/main/java/AlgorithmAndMath/Main020.java
+++ src/main/java/AlgorithmAndMath/Main020.java
@@ -1,5 +1,7 @@
 package src.main.java.AlgorithmAndMath;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main020 {
@@ -7,23 +9,40 @@ public class Main020 {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int[] sortedInputArray = new int[N];
-
+        ArrayList<Integer> inputArray = new ArrayList<>();
         int ans = 0;
         final int TARGET_VALUE = 1000;
         for (int i = 0; i < N; i++) {
-            sortedInputArray[i]=scanner.nextInt();
+            inputArray.add(scanner.nextInt());
+        }
+        Collections.sort(inputArray, Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            sortedInputArray[i] = inputArray.get(i);
         }
 
         for (int i = 0; i <= N-5; i++) {
 
             for (int j = i+1; j <= N-4; j++) {
                 int sum_i_j = sortedInputArray[i]+ sortedInputArray[j];
+                if(sum_i_j >= TARGET_VALUE){
+                    continue;
+                }
                 for (int k = j+1; k <= N-3; k++) {
                     int sum_i_j_k = sum_i_j + sortedInputArray[k];
+                    if(sum_i_j_k >= TARGET_VALUE){
+                        continue;
+                    }
                     for (int l = k+1; l <= N-2; l++) {
                         int sum_i_j_k_l = sum_i_j_k + sortedInputArray[l];
+                        if(sum_i_j_k_l >= TARGET_VALUE){
+                            continue;
+                        }
                         for (int m = l+1; m <= N-1; m++) {
-                            if(sum_i_j_k_l + sortedInputArray[m] == TARGET_VALUE){
+                            int sum_i_j_k_l_m = sum_i_j_k_l + sortedInputArray[m];
+                            if(sum_i_j_k_l_m > TARGET_VALUE){
+                                continue;
+                            }
+                            if(sum_i_j_k_l_m == TARGET_VALUE){
                                 ans++;
                             }
                         }
