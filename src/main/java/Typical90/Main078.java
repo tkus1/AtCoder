b73cf6c002b838936b7e75f37cdda7aa03a01316
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-01-15 00:58:09 -0800

    Typical90 64 and 67

---
+++ src/main/java/Typical90/Main078.java
@@ -0,0 +1,13 @@
+package src.main.java.Typical90;
+
+import java.util.Scanner;
+
+public class Main078 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+
+    }
+}
+
