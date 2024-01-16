a8924d6d2ddd1814811c627a1059e38a0415a4bb
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-01-15 21:38:32 -0800

    Typical90 82, 84 and 85

--- src/main/java/Typical90/Main085.java
+++ src/main/java/Typical90/Main085.java
@@ -1,12 +1,48 @@
 package src.main.java.Typical90;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main085 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
+        long N = scanner.nextLong();
+        ArrayList<Long> divisors = getDivisors(N);
+        int ansCnt = 0;
+        for (int i = 0; i < divisors.size(); i++) {
+            long a = divisors.get(i);
+            for (long b : divisors) {
+                if (a > b) {
+                    continue;
+                }
 
+                if (N % a  != 0) {
+                    continue;
+                }
+                long NdivA = N / a;
+                if (NdivA % b != 0) {
+                    continue;
+                }
+
+                long c = NdivA / b;
+                if (b > c) {
+                    continue;
+                }
+                ansCnt++;
+            }
+        }
+        System.out.println(ansCnt);
+    }
+    public static ArrayList<Long> getDivisors(long num){
+        ArrayList<Long> divisors = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(num); i++) {
+            if(num % i == 0){
+                divisors.add((long)i);
+                if(i != num/i){
+                    divisors.add((long)num/i);
+                }
+            }
+        }
+        return divisors;
     }
 }
